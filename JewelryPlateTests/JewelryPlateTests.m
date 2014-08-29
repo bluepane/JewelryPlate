21f1441542eef235919d7cb3ed5325c79528aa35
bluepane <qingchen@AILK-yangjz.local>
2014-08-29 11:36:09 +0800

    框架
    创建框架

---
+++ JewelryPlateTests/JewelryPlateTests.m
@@ -0,0 +1,34 @@
+//
+//  JewelryPlateTests.m
+//  JewelryPlateTests
+//
+//  Created by yangjz on 14-3-21.
+//  Copyright (c) 2014年 yangjz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JewelryPlateTests : XCTestCase
+
+@end
+
+@implementation JewelryPlateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
